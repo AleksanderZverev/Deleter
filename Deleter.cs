c8f6cdd7422647fb7a8e3ab8cd6dfb5aeaec1061
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2020-08-23 16:14:36 +0500

    initial commit

---
+++ Deleter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Deleter
+{
+    public static class Deleter
+    {
+        public static void FindAndDeleteAllFiles(DirectoryInfo dir, IReadOnlyList<string> fileNames)
+        {
+            Console.WriteLine("Enter in the \"" + dir.Name + "\" directory");
+            var directories = dir.GetDirectories();
+            foreach (var directoryInfo in directories)
+            {
+                if (fileNames.Contains(directoryInfo.Name))
+                {
+                    DeleteDirectory(directoryInfo);
+                    Console.WriteLine("-" + directoryInfo.Name + " - deleted");
+                }
+                else
+                    FindAndDeleteAllFiles(directoryInfo, fileNames);
+            }
+        }
+
+        public static void DeleteDirectory(DirectoryInfo directory)
+        {
+            foreach (var fileInfo in directory.GetFiles())
+            {
+                fileInfo.Delete();
+            }
+
+            foreach (var directoryInfo in directory.GetDirectories())
+            {
+                DeleteDirectory(directoryInfo);
+            }
+
+            directory.Delete();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Deleter
+{
+    class Program
+    {
+        private static readonly List<string> fileNames = new List<string>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите имя файла, который хотите удалить.");
+                var answer = Console.ReadLine();
+                if (answer == null) break;
+                fileNames.Add(answer);
+
+                Console.WriteLine("Еще? (y/n)");
+                var againAns = Console.ReadLine();
+                if (againAns == null || againAns.ToLower() != "y")
+                    break;
+            }
+
+            Console.WriteLine("Введенные имена файлов: " + string.Join(", ", fileNames) + ".");
+            Console.WriteLine("Приступить к удалению?(y/n)");
+            var ans = Console.ReadLine();
+            if (ans == null || ans.ToLower() != "y")
+                return;
+
+            Console.WriteLine("Приступаю к удалению!");
+            Console.WriteLine(new string('-', 20));
+
+            var path = Directory.GetCurrentDirectory();
+            var dir = new DirectoryInfo(path);
+            Deleter.FindAndDeleteAllFiles(dir, fileNames);
+
+            Console.WriteLine();
+            Console.WriteLine("Готово!");
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть программу...");
+            Console.ReadLine();
+        }
+    }
+}
